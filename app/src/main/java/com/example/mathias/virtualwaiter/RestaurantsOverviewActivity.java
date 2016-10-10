aa844e3844683a361c2a8b1f2ebc026cd28aefe6
solveigdoan <solveig.doan@hotmail.com>
2016-10-10 15:59:41 +0200

    Update RestaurantsOverviewActivity.java

--- app/src/main/java/com/example/mathias/virtualwaiter/RestaurantsOverviewActivity.java
+++ app/src/main/java/com/example/mathias/virtualwaiter/RestaurantsOverviewActivity.java
@@ -8,12 +8,23 @@ import android.view.MotionEvent;
 public class RestaurantsOverviewActivity extends AppCompatActivity {
     private float x1,x2;
     static final int MIN_DISTANCE = 150;
+    
+     private ListView list_restaurants_overview;
+    private RestaurantListAdapter adapter;
+    private List <RestaurantModel> RestaurantModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_overview);
+        
+        list_restaurants_overview = (ListView)findViewById(R.id.list_restaurant_overview);
+        RestaurantModelList = new ArrayList<>();
+        adapter = new RestaurantListAdapter(getApplicationContext(), RestaurantModelList);
+        list_restaurants_overview.setAdapter(adapter);
+
     }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event)
